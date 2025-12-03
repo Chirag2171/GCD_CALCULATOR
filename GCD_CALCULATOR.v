5f4af4f8807f2d5f8f64b0f990c299b49077f322
Chirag2171 <chiragrao2171@gmail.com>
2025-12-03 11:13:46 +0530

    Update GCD_CALCULATOR.v

--- GCD_CALCULATOR.v
+++ GCD_CALCULATOR.v
@@ -1,1 +1,38 @@
-
+GCD_PROCESSOR (clk, reset, INPUT_X, INPUT_Y, DONE, OUTPUT);
+	input wire clk, reset;
+	input wire [7:0] INPUT_X, INPUT_Y;
+	output wire DONE;
+	output wire [7:0] OUTPUT;
+	wire IN_X, IN_Y, LOAD_X, LOAD_Y, XY, OUT_DP, XEQY, XGTY;
+	
+	GCD_DP myGCD_DP (
+		.clk(clk), 
+		.reset(reset), 
+		.INPUT_X(INPUT_X), 
+		.INPUT_Y(INPUT_Y), 
+		.IN_X(IN_X), 
+		.IN_Y(IN_Y), 
+		.LOAD_X(LOAD_X), 
+		.LOAD_Y(LOAD_Y), 
+		.XY(XY), 
+		.OUT_DP(OUT_DP), 
+		.XEQY(XEQY), 
+		.XGTY(XGTY), 
+		.OUTPUT(OUTPUT)
+	);
+	
+	GCD_CU myGCD_CU (
+		.clk(clk), 
+		.reset(reset), 
+		.XEQY(XEQY), 
+		.XGTY(XGTY), 
+		.IN_X(IN_X), 
+		.IN_Y(IN_Y), 
+		.LOAD_X(LOAD_X), 
+		.LOAD_Y(LOAD_Y), 
+		.XY(XY), 
+		.OUT_DP(OUT_DP), 
+		.DONE(DONE)
+	);
+	
+endmodule
