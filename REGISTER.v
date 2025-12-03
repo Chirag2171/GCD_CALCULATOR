da6241c2c3faea0984e6ea686f0a883116d0d9d2
Chirag2171 <chiragrao2171@gmail.com>
2025-12-03 11:18:13 +0530

    Update REGISTER.v

--- REGISTER.v
+++ REGISTER.v
@@ -1,1 +1,14 @@
-
+module REGISTER (clk, reset, LOAD, D, Q);
+	input clk, reset, LOAD;
+	input [7:0] D;
+	output reg [7:0] Q;
+	
+	always @(posedge clk, posedge reset)
+	begin
+		if (reset)
+			Q <= 8'b0;
+		else if (LOAD)
+			Q <= D;
+	end
+	
+endmodule
